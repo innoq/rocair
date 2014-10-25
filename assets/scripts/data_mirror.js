c01835d6f50986dd5308f5b49367951ada9af215
Roman Stranghoener <roman.stranghoener@innoq.com>
2014-10-25 17:45:42 +0200

    add minor improvements and fixes

--- assets/scripts/data_mirror.js
+++ assets/scripts/data_mirror.js
@@ -11,6 +11,7 @@ $.fn.dataMirror = function () {
 		var receiverID = emitter.attr("data-emitter");
 		var receiversSelector = "[data-receiver=" + receiverID + "]";
 		var receivers = $(receiversSelector);
+		receivers.attr('aria-live', 'polite');
 		emitter.on('click', function(ev){
 			var value = $(this).find('input').attr('value');
 			receivers.each(function(){
